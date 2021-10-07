4c098e9abacd5ab8c1a6aff246b7eb3ff7c0acec
Kiwi <agiannoulis@mavestro.co>
2021-10-07 21:29:10 +0300

    .

--- services/shape.go
+++ services/shape.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"context"
+	"errors"
 	"github.com/ProjectAthenaa/shape"
 	protos "github.com/ProjectAthenaa/sonic-core/sonic/antibots/shape"
 )
@@ -10,10 +11,16 @@ type Server struct {
 	protos.UnimplementedShapeServer
 }
 
-func (s Server) GenHeaders(ctx context.Context, site *protos.Site) (*protos.Headers, error) {
+func (s Server) GenHeaders(ctx context.Context, site *protos.Site) (headers *protos.Headers, err error) {
+	err = nil
+	defer func() {
+		if a := recover(); a != nil {
+			err = errors.New("internal_error")
+		}
+	}()
 	if site.ResString != nil {
-		return &protos.Headers{Values: shape.GenerateHeaders(site.Value, *site.ResString)}, nil
+		return &protos.Headers{Values: shape.GenerateHeaders(site.Value, *site.ResString)}, err
 	}
 
-	return &protos.Headers{Values: shape.GenerateHeaders(site.Value)}, nil
+	return &protos.Headers{Values: shape.GenerateHeaders(site.Value)}, err
 }
